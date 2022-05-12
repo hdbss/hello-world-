2c69105786bc05be115c7f21befee7a2b23f63cc
hdbss <70868499+hdbss@users.noreply.github.com>
2022-05-12 21:26:47 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,12 +5,6 @@ def func():
     print(1)
     yield 2
 
-@pytest.mark.usefixtures('1')
-def test_123():
-    print(223)
-
-def opo():
-    return 12321
 
 if __name__ == "__main__":
     pytest.main(['-s'])
